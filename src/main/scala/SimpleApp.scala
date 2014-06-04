b823877f69389d1db7d16301cd5b5dd8776cd57f
himanshu <himanshu@knoldus.com>
2014-06-04 14:33:27 +0530

    init

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -8,8 +8,7 @@ object SimpleApp {
     val sc = new SparkContext("local", "Simple App", "/path/to/spark-0.9.1-incubating",
       List("target/scala-2.10/simple-project_2.10-1.0.jar"))
     val logData = sc.textFile(logFile, 2).cache()
-    val numAs = logData.filter(line => line.contains("a")).count()
-    val numBs = logData.filter(line => line.contains("b")).count()
-    println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    val numTHEs = logData.filter(line => line.contains("the")).count()
+    println("Lines with the: %s".format(numTHEs))
   }
 }
